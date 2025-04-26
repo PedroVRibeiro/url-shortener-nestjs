b02922bbdc91b05e5dc98dd1cdf682c6ff79d88f
PedroVRibeiro <ribeiro.tpedro@gmail.com>
2025-04-26 11:25:41 -0300

    feat: deploy setup

--- src/config/database/datasource.ts
+++ src/config/database/datasource.ts
@@ -3,13 +3,26 @@ import * as dotenv from 'dotenv';
 
 dotenv.config({ path: process.env.DOTENV_CONFIG_PATH || '.env' });
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
-  database: process.env.DATABASE_NAME,
-  entities: ['dist/**/*.entity.{ts,js}'],
-  migrations: ['dist/migrations/*.{ts,js}'],
-});
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const AppDataSource = new DataSource(
+  isProduction
+    ? {
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        entities: ['dist/**/*.entity.{ts,js}'],
+        migrations: ['dist/migrations/*.{ts,js}'],
+        synchronize: false,
+      }
+    : {
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_NAME,
+        entities: ['dist/**/*.entity.{ts,js}'],
+        migrations: ['dist/migrations/*.{ts,js}'],
+        synchronize: false,
+      }
+);
