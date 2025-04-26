3616d3a56464a53dd5ce41b4ee18728ddcdc4a5f
PedroVRibeiro <ribeiro.tpedro@gmail.com>
2025-04-26 11:40:06 -0300

    fix: minor fixes

--- src/config/database/datasource.ts
+++ src/config/database/datasource.ts
@@ -24,5 +24,5 @@ export const AppDataSource = new DataSource(
         entities: ['dist/**/*.entity.{ts,js}'],
         migrations: ['dist/migrations/*.{ts,js}'],
         synchronize: false,
-      }
+      },
 );
